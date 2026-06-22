39997418a8a9a7e3c2be0720fe4e1b642d9efe84
Aman Pushp <amanpushp2001@gmail.com>
2026-06-22 09:00:42 +0000

    add home page

--- myapp/urls.py
+++ myapp/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("🚀 Jenkins + DockerHub + Cloudflare Tunnel CI/CD Working Successfully")
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
 ]
